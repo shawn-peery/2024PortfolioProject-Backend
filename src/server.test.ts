c393710387b4862ec0a3eb6be64f2d6f5b83977b
Shawn Peery <shawn.w8465@gmail.com>
2024-07-21 12:39:19 -0600

    Third test. text response root url

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,19 +1,21 @@
 import { describe, it, expect } from 'vitest'
+import request from "supertest"
 import { app, port } from './server';
 
 import express from 'express'
 
-
-
 describe('Server Tests', () => {
 	it('should create an Express app instance', () => {
 		expect(app).toBeDefined();
-		console.log(`App's constructor: ${app.constructor}`)
-		console.log(`Express()'s constructor: ${express.constructor}`)
 		expect(app.constructor).toBe((express()).constructor);
 	});
 	it("should have the server listening on port 5000", () => {
 		expect(port).toBe(5000);
+	})
+	it("should return a message when requesting root url", () => {
+		return request(app).get('/').expect('Content-Type', /text/).expect(200).then(response => {
+			expect(response.text).toEqual("Successfully received message!");
+		})
 	})
 })
 
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,13 @@ const app = express();
 
 const port = 5000;
 
+app.get('/', (req, res) => {
+	res.contentType('text');
+
+	res.send("Successfully received message!")
+
+})
+
 app.listen(port, () => {
 })
 
