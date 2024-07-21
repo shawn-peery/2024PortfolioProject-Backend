fb4e8e730fa90c7316a8ba0fa62dbea4fac067b3
Shawn Peery <shawn.w8465@gmail.com>
2024-07-21 12:19:10 -0600

    First passing test. Express instance
    - Testing that the express instance is created

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,13 +1,16 @@
 import { describe, it, expect } from 'vitest'
-import { app, port } from './server';
+import { app, } from './server';
 
 import express from 'express'
+
 
 
 describe('Server Tests', () => {
 	it('should create an Express app instance', () => {
 		expect(app).toBeDefined();
-		expect(app).toBeInstanceOf(express());
+		console.log(`App's constructor: ${app.constructor}`)
+		console.log(`Express()'s constructor: ${express.constructor}`)
+		expect(app.constructor).toBe((express()).constructor);
 	});
 })
 
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,5 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 
 const app = express();
 
-const port = 5000;
-
-app.get('/', (req: Request, res: Response) => {
-	res.send("Hello, Typescript + Node.js + Express!")
-})
-
-app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`)
-})
-
+export { app };
